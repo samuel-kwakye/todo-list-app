1d0c3aca78e806fd515cc1464108a7ad97600fe3
Samuel Osei Kwkaye <samuel-kwakye@turntabl.io>
2020-05-08 12:29:03 +0000

    Setting UI for Home & Todo Page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 
+
 # initialize instance of WSGI application
 # act as a central registry for the view functions, URL rules, template configs
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('hello.html')
+    homeActive = "active"
+    return render_template('home.html', homeActive=homeActive)
+
+@app.route("/todo")
+def todo():
+    todoActive = "active"
+    return render_template("todo.html", todoActive=todoActive)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7000, debug=True)
---
+++ services.py
@@ -0,0 +1,6 @@
+class TodoService:
+
+    @staticmethod
+    def get_all_todos:
+        #Database query and logic implementation
+        return None
